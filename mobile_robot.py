ea08935389694310bd348cb08ae5d0d46c82c994
Ignacio Ricart <ignacio@poli.ufrj.br>
2021-01-04 01:23:56 +0100

    create update method

--- mobile_robot.py
+++ mobile_robot.py
@@ -1,7 +1,7 @@
-import rotation
+from rotation import Rotation
 import numpy as np
 class MobileRobot:
-    def __init__(self, state: np.array, dt: float):
+    def __init__(self, state: np.array, dt: float, wheel_radius: float = 1.0, distance_between_wheel_and_rotation_point: float = 1.0) -> None:
         if state.size != 3:
             print("Current number of states: ", state.size,". Mobile Robot should have 3 states...")
             exit()
@@ -11,13 +11,32 @@ class MobileRobot:
             self._y = state[1]
             # reference in odom
             self._theta = state[2]
+            self._r = wheel_radius
+            self._l = distance_between_wheel_and_rotation_point # distance from robot center of rotation and wheel
         
-    def update(self, input: np.array):
-        # considering it is in the same reference frame
-        # input is linear velocity and theta
+    def update_local_frame(self, input: np.array) -> np.array:
+        # input is velocity on each wheel
+        if input.size == 2:
+            omega_right = input[0] * self._r / (2 * self._l)
+            omega_left = - input[1] * self._r / (2 * self._l)
+
+            return np.array([[self._r * (input[0] + input[1]) / 2],
+                                [0], 
+                                [omega_right + omega_left]])
+        else:
+            print("There should be a np.array of size 2 as input...")
+            exit()
+
+    def update(self, input: np.array) -> np.array:
+        local_frame_states = self.update_local_frame(input)
+        rot = Rotation(self._theta)
+        odom_frame_states = np.matmul(rot.RotZ3D.transpose(), local_frame_states)
         
-        self._theta += input[2]
+        #print("My local frame states are:\n", local_frame_states)
+        #print("My odom frame states are:\n",odom_frame_states)
+        
+        return odom_frame_states
 
     @property
     def states(self) -> np.array:
-        return np.array([self._x, self._y, self._theta])
+        return np.array([self._x, self._y, self._theta, self._dt, self._r, self._l,])
--- test_mobile_robot.py
+++ test_mobile_robot.py
@@ -4,27 +4,36 @@ import numpy as np
 import unittest
 
 class Test(unittest.TestCase):
-    def test_rotation_from_odom_to_reference_frame(self):
-        # map motion in global frame (_I) to local frame (_r)
-        # xi_r = R(theta) * xi_I
-        initial_condition = np.array([0, 0, 0])
-        mr = MobileRobot(initial_condition, 0.1)
-        mr.update(np.array([0,0,1]))
-        mr.update(np.array([0,0,1]))
-        print(mr.states)
-        mr.update(np.array([0,0,1]))
-        mr.update(np.array([0,0,1]))
-        print(mr.states)
-        #self.assertAlmostEqual(result, expected_result)
+    def test_mobile_robot_constructor(self):
+        print("Test Constructor")
+        mr = MobileRobot(np.array([0, 0, np.deg2rad(90)]), 0.1, 1, 1)
+        np.testing.assert_array_equal([0, 0, np.deg2rad(90), 0.1, 1, 1], mr.states)
+        vel_input = np.array([4, 2])
+        mr.update(vel_input)
 
-    def test_mobile_robot_rotation(self):
-        initial_condition = np.array([0, 0, 0])
-        mr = MobileRobot(initial_condition, 0.1)
-        mr.update(np.array([0,0,1]))
-        mr.update(np.array([0,0,1]))
-        print(mr.states)
-        mr.update(np.array([0,0,1]))
-        mr.update(np.array([0,0,1]))
-        print(mr.states)
+    # def test_rotation_from_odom_to_reference_frame(self):
+        
+    #     # map motion in global frame (_I) to local frame (_r)
+    #     # xi_r = R(theta) * xi_I
+    #     initial_condition = np.array([0, 0, 0])
+    #     mr = MobileRobot(initial_condition, 0.1)
+    #     mr.update(np.array([0,0,1]))
+    #     mr.update(np.array([0,0,1]))
+    #     print(mr.states)
+    #     mr.update(np.array([0,0,1]))
+    #     mr.update(np.array([0,0,1]))
+    #     print(mr.states)
+    #     #self.assertAlmostEqual(result, expected_result)
+
+    # def test_mobile_robot_rotation(self):
+    #     initial_condition = np.array([0, 0, 0])
+    #     mr = MobileRobot(initial_condition, 0.1, 1, 1)
+    #     mr.update(np.array([0,0,1]))
+    #     mr.update(np.array([0,0,1]))
+    #     print(mr.states)
+    #     mr.update(np.array([0,0,1]))
+    #     mr.update(np.array([0,0,1]))
+    #     print(mr.states)
+
 
 unittest.main()
