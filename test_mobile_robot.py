ac4d3fc224b05a6992fb2537f48924e2ea471154
Ignacio Ricart <ignacio@poli.ufrj.br>
2021-01-04 04:17:29 +0100

    clean test

--- test_mobile_robot.py
+++ test_mobile_robot.py
@@ -16,18 +16,12 @@ class Test(unittest.TestCase):
         # (v_r - v_l) * t / ( distance_between_wheel_and_rotation_point * 2) = 2 * pi => distance_between_wheel_and_rotation_point = 10 / pi
         mr = MobileRobot(np.array([0, 0, 0]), 0.1, 1, 2 / np.pi)
         vel_input = np.array([2, -2])
-        result_x = []
-        result_y = []
-        result_theta = []
-        result_time = []
         for i in range(10):
             i += 1
             output = mr.update_method_integral(vel_input)
-            result_x.append(output[0])
-            result_y.append(output[1])
-            result_theta.append(output[2])
-            result_time.append(output[3])
-            print(output)
+            np.testing.assert_array_equal(output[0], 0)
+            np.testing.assert_array_equal(output[1], 0)
+            np.testing.assert_array_almost_equal(output[2], i*np.pi/10, decimal=2)
             pass
 
     def test_mobile_robot_update_with_integral_method_move_around_initial_pose_is_zero(self):
@@ -49,31 +43,23 @@ class Test(unittest.TestCase):
             print(output)
             pass
 
-
-
-    # def test_rotation_from_odom_to_reference_frame(self):
+    def test_mobile_robot_update_with_integral_method_initial_pose_different_than_zero(self):
+        print("Move around with zero initial pose")
+        # (v_r - v_l) * t / ( distance_between_wheel_and_rotation_point * 2) = 2 * pi => distance_between_wheel_and_rotation_point = 10 / pi
+        mr = MobileRobot(np.array([0, 0, 0]), 0.2, 1, 2 / np.pi)
+        vel_input = np.array([3, 1])
+        result_x = []
+        result_y = []
+        result_theta = []
+        result_time = []
+        for i in range(10):
+            i += 1
+            output = mr.update_method_integral(vel_input)
+            result_x.append(output[0])
+            result_y.append(output[1])
+            result_theta.append(output[2])
+            result_time.append(output[3])
+            print(output)
+            pass
         
-    #     # map motion in global frame (_I) to local frame (_r)
-    #     # xi_r = R(theta) * xi_I
-    #     initial_condition = np.array([0, 0, 0])
-    #     mr = MobileRobot(initial_condition, 0.1)
-    #     mr.update(np.array([0,0,1]))
-    #     mr.update(np.array([0,0,1]))
-    #     print(mr.states)
-    #     mr.update(np.array([0,0,1]))
-    #     mr.update(np.array([0,0,1]))
-    #     print(mr.states)
-    #     #self.assertAlmostEqual(result, expected_result)
-
-    # def test_mobile_robot_rotation(self):
-    #     initial_condition = np.array([0, 0, 0])
-    #     mr = MobileRobot(initial_condition, 0.1, 1, 1)
-    #     mr.update(np.array([0,0,1]))
-    #     mr.update(np.array([0,0,1]))
-    #     print(mr.states)
-    #     mr.update(np.array([0,0,1]))
-    #     mr.update(np.array([0,0,1]))
-    #     print(mr.states)
-
-
 unittest.main()
